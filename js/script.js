34257a0510c3e0fc7150c618bd6e855215c0d0fc
abdicore <abdirhamanmohamed@gmail.com>
2024-08-26 23:51:21 +0200

    mailstone2-ok

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ Se riusciamo, facciamo anche in modo da non poter più cliccare sulla stessa cel
 # MILESTONE 2
 Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
 Generiamoli e stampiamo in console per essere certi che siano corretti
-# MILESTONE 3
+# MILESTONE 3 
 Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe.
 Se si, la cella diventa rossa (raccogliamo il punteggio e scriviamo in console che la patita termina) altrimenti diventa azzurra e dobbiamo 
 incrementare il punteggio.
